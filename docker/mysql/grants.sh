9e380f1a5f3fed8b4da8ccdecb73229c1f40144a
Bruno Rocha <brunoalvesrocha@icloud.com>
2019-04-16 16:31:38 -0300

    Update grants.sh
    update grafana password

--- docker/mysql/grants.sh
+++ docker/mysql/grants.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 3
-mysql -uroot -ppetclinic -e “grant all privileges on grafana.* to ‘root’@‘%’ identified by ‘grafana’;”
+mysql -uroot -pgrafana123456 -e “grant all privileges on grafana.* to ‘root’@‘%’ identified by ‘grafana’;”
 sleep 2
-mysql -uroot -ppetclinic -e “flush privileges;”
+mysql -uroot -pgrafana123456 -e “flush privileges;”
